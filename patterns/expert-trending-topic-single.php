8e23b75aab651f9cd5e0a2af84ca764049e99a71
Mark Naylor <mnaylor@utk.edu>
2026-07-31 15:01:33 -0500

    style(trending topic): adjust trending topic card layout and style

--- patterns/expert-trending-topic-single.php
+++ patterns/expert-trending-topic-single.php
@@ -15,41 +15,37 @@
 
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained"},"className":"trending-expert-group"} -->
+<!-- wp:group {"layout":{"type":"default"},"className":"trending-expert-group"} -->
 <div class="trending-expert-group wp-block-group">
-	<!-- wp:image {"width":"240px","height":"auto","scale":"cover","align":"right","className":"is-style-default trending-expert-figure"} -->
-	<figure class="trending-expert-figure wp-block-image alignright is-resized has-custom-border is-style-default">
-		<img 
-			src="<?php echo esc_url( get_theme_file_uri( 'assets/images/person-placeholder.jpeg' ) ); ?>" 
-			alt="person placeholder" 
+
+	<!-- wp:heading {"level":3,"className":"trending-expert-topic"} -->
+	<h3 class="trending-expert-topic wp-block-heading">Topic</h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:separator {"className":"trending-expert-rule"} -->
+	<hr class="wp-block-separator trending-expert-rule"/>
+	<!-- /wp:separator -->
+
+	<!-- wp:image {"scale":"cover","align":"right","className":"is-style-default trending-expert-figure"} -->
+	<figure class="trending-expert-figure wp-block-image alignright is-style-default">
+		<img
+			src="<?php echo esc_url( get_theme_file_uri( 'assets/images/person-placeholder.jpeg' ) ); ?>"
+			alt="person placeholder"
 		/>
 	</figure>
 	<!-- /wp:image -->
 
-	<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}, "classname":"trending-expert-text"} -->
-	<div class="trending-expert-text wp-block-group">
-		<!-- wp:heading {"level":3, "className":"trending-expert-topic"} -->
-		<h3 class="trending-expert-topic wp-block-heading">Topic</h3>
-		<!-- /wp:heading -->
+	<!-- wp:heading {"level":4,"className":"trending-expert-name"} -->
+	<h4 class="trending-expert-name wp-block-heading">Expert Name</h4>
+	<!-- /wp:heading -->
 
-		<!-- wp:separator -->
-		<hr class="wp-block-separator"/>
-		<!-- /wp:separator -->
+	<!-- wp:paragraph {"className":"trending-expert-desc"} -->
+	<p class="trending-expert-desc">A contact card to display an expert in the trending topics section of the expert's guide. Edit topic, name, description, link and photo to display an expert.</p>
+	<!-- /wp:paragraph -->
 
-		<!-- wp:heading {"level":4, "className":"trending-expert-name"} -->
-		<h4 class="trending-expert-name wp-block-heading">Expert Name</h4>
-		<!-- /wp:heading -->
+	<!-- wp:paragraph {"className":"is-style-utkwds-single-link"} -->
+	<p class="is-style-utkwds-single-link"><a href="#">View expert profile</a></p>
+	<!-- /wp:paragraph -->
 
-		<!-- wp:paragraph -->
-		<p>A contact card to display an expert in the trending topics section of the expert's guide. Edit topic, name, description, link and photo to display an expert.</p>
-		<!-- /wp:paragraph -->
-
-		<!-- wp:paragraph {"className":"is-style-utkwds-single-link"} -->
-		<p class="is-style-utkwds-single-link">
-			<a href="#">View expert profile</a>
-		</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
